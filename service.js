1d03d1df3a8a931e921fc708dd175768666704e0
Mattias Petter Johansson <mpj@spotify.com>
2014-08-05 11:24:27 +0200

    trying with express

--- service.js
+++ service.js
@@ -1,13 +1,31 @@
-
+var express = require('express')
 var proxyServer = require('http-route-proxy');
 
 var port = process.env.PORT || 3000;
+
+app.use(proxyServer.connect({
+  to: 'p.scdn.co',
+  route: ['/']
+}));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+/*
 proxyServer.proxy([
     {
         from: 'uncovered-proxy.herokuapp.com',
         to: 'p.scdn.co',
     }
-]);
+]);*/
 
 
 
